21851e19bd1cd2f9b9be8a81fa62c90e420d7db9
El3Du4Rd0 <a01710168@tec.mx>
2024-08-08 09:18:26 -0600

    commit 3

--- Actividad 1.1. Planteamiento e Implementación de algoritmos/Actividad.py
+++ Actividad 1.1. Planteamiento e Implementación de algoritmos/Actividad.py
@@ -1,36 +1,9 @@
-"""
-Se desea calcular los primeros 15 números de la siguiente sumatoria:
-Suma = 1 + 1/2² + 1/3³ + 1/4² + 1/5⁵ + 1/6² + ...
-"""
-def sumatoria(n):
-    res = 0
-    if n == 1:
-        return 1
-    else:
-        for i in range(1, n + 1):
-            if (i % 2) == 0:
-                res += (1/i**2)
-            else:
-                res += (1/i**i)
-        return res
-
 from random import *
 
-# Se desea calcular el factorial de un número dado por el usuario.
-def factorial(n):
-    if n == 1 or n == 0:
-        return 1
-    else:
-        res = 1
-        for i in range(1, n + 1):
-            res *= i
-        return res
-    
-x = int(input())
-print(factorial(x))
-
-''' 3. Que solicite un número que indica cuántos números aleatorios (positivos y
-negativos) se mostrarán.'''
+'''
+3. Que solicite un número que indica cuántos números aleatorios (positivos y
+negativos) se mostrarán.
+'''
 print("Ingresa el numero inferior: ")
 num_infer = int(input())
 
@@ -41,8 +14,10 @@ random_num = randint(num_infer, num_super)
 print(random_num)
 
 
-'''4. Se muestren todos los números, la suma y el promedio de la siguiente serie
-3, 6, 9, 12,… 99. '''
+'''
+4. Se muestren todos los números, la suma y el promedio de la siguiente serie
+3, 6, 9, 12,… 99. 
+'''
 lista = []
 i = 3
 total = 0
@@ -60,3 +35,34 @@ prom = total / count
 print(lista)
 print(total)
 print(prom)
+
+"""
+5. Se desea calcular los primeros 15 números de la siguiente sumatoria:
+Suma = 1 + 1/2² + 1/3³ + 1/4² + 1/5⁵ + 1/6² + ...
+"""
+def sumatoria(n):
+    res = 0
+    if n == 1:
+        return 1
+    else:
+        for i in range(1, n + 1):
+            if (i % 2) == 0:
+                res += (1/i**2)
+            else:
+                res += (1/i**i)
+        return res
+
+print("Resultado: " + str(sumatoria(15)))
+
+# 6. Se desea calcular el factorial de un número dado por el usuario.
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        res = 1
+        for i in range(1, n + 1):
+            res *= i
+        return res
+    
+numFactorial = int(input("Factorial: "))
+print("Resultado: " + str(factorial(numFactorial)))
