cff4d5939cc9832f98f62059499c0a87914cd8d7
Johnccg <150750774+Johnccg@users.noreply.github.com>
2024-08-12 12:19:19 -0600

    Corregir y terminar programa de puntos cercanos

--- Actividad_DivideVencer/PuntosCercanos.py
+++ Actividad_DivideVencer/PuntosCercanos.py
@@ -13,18 +13,17 @@ Actividad 1.3: Aplicaciones de la técnica de programación "divide y vencerás"
 from math import sqrt
 
 # Constantes
-PUNTOS = [(1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8), (8,9), (9,0), (0,0)]
+PUNTOS = [(0, 21),(4, 3),(32, 30),(16, 26),(23, 48),(36, 15),(40, 16),(45, 38),(35, 26),(19, 31),(28, 28),(33, 13),(33, 17),(37, 23),(18, 11),(47, 21),(12, 40),(34, 9),(3, 32),(20, 45)]
 ORDEN_X = sorted(PUNTOS, key=lambda p: p[0])
 ORDEN_Y = sorted(PUNTOS, key=lambda p: p[1])
-
 # Funcion para obtener la distancia entre dos puntos
 def distancia(punto1, punto2):
     res = sqrt( ( ( punto1[0] - punto2[0] ) ** 2) + ( ( punto1[1] - punto2[1] ) ** 2) )
     return res
 
 # Funcion recursiva para obtener la distancia minima entre dos puntos
-def parCercano(puntosX, puntosY):
-    n = len(puntosX)
+def parCercano(puntos):
+    n = len(puntos)
 
     # Caso base de nuetra funcion recursiva
     if n <= 3:
@@ -32,28 +31,28 @@ def parCercano(puntosX, puntosY):
         p1, p2 = None, None
 
         for i in range(n):
-            dist = distancia(puntosX[i], puntosY[i])
-            if dist < mini:
-                mini = dist
-                p1, p2 = puntosX[i], puntosY[i]
+            for j in range(i + 1, n):
+                dist = distancia(puntos[i], puntos[j])
+                if dist < mini:
+                    mini = dist
+                    p1, p2 = puntos[i], puntos[j]
         return p1, p2, mini
     
     micha = n // 2
-    puntoM = puntosX[micha]
+    puntoM = puntos[micha]
 
-    IzqX = list( filter( lambda p: p[0] <= puntoM[0], puntosX ) )
-    DerX = list( filter( lambda p: p[0] > puntoM[0], puntosX ) )
+    PuntosIzq = list( filter( lambda p: p[0] <= puntoM[0], puntos ) )
+    PuntosDer = list( filter( lambda p: p[0] > puntoM[0], puntos ) )
 
-    IzqY = list( filter( lambda p: p[0] <= puntoM[0], puntosY ) )
-    DerY = list( filter( lambda p: p[0] > puntoM[0], puntosY ) )
+    (p1_Izq, p2_Izq, dist_Izq) = parCercano(PuntosIzq)
+    (p1_Der, p2_Der, dist_Der) = parCercano(PuntosDer)
 
-    (p1_Izq, p2_Izq, dist_Izq) = parCercano(IzqX, IzqY)
-    (p1_Der, p2_Der, dist_Der) = parCercano(DerX, DerY)
+    #print(p1_Izq, p2_Izq, dist_Izq, p1_Der, p2_Der, dist_Der)
 
     min_dist = min(dist_Izq, dist_Der)
     (p1, p2) = (p1_Izq, p2_Izq) if dist_Izq < dist_Der else (p1_Der, p2_Der)
 
-    res = [p for p in puntosY if abs(p[0] - puntoM[0]) < min_dist]
+    res = [p for p in puntos if abs(p[0] - puntoM[0]) < min_dist]
 
     for i in range(len(res)):
         for j in range( i + 1, min(i + 7, len(res)) ):
@@ -64,7 +63,7 @@ def parCercano(puntosX, puntosY):
 
     return p1, p2, min_dist
 
-print(parCercano(ORDEN_X, ORDEN_Y))
+print(parCercano(ORDEN_X))
 
 
 
