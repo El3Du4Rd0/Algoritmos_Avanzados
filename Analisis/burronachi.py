befaa4608528380c90163449bb335eeddf1ad332
Pablo H <h.h.m.pablo@gmail.com>
2024-09-01 23:16:28 -0600

    .

--- Analisis/burronachi.py
+++ Analisis/burronachi.py
@@ -4,8 +4,8 @@ def fibonacci_PD (n):
     f.append(1)
     if (n > 0): 
         f.append(2)
-        for i in range(2,n + 1):
+        for i in range(2,n):
             f.append(((4 * f[i - 1]) + (2 * f[i - 2]))/2)
-    return f
+    return f[n]
 
 print(fibonacci_PD(8))
