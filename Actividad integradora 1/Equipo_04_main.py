f4515d12f52b36665a737ae27eb4c6df6982336f
Pablo H <h.h.m.pablo@gmail.com>
2024-11-10 23:00:49 -0600

    Co-authored-by: El3Du4Rd0 <El3Du4Rd0@users.noreply.github.com> Co-authored-by: Juan Carlos Calderón <Johnccg@users.noreply.github.com> Co-authored-by: Juan Eduardo Rosas Ceron <El3Du4Rd0@users.noreply.github.com>

--- Actividad integradora 1/Equipo_04_main.py
+++ Actividad integradora 1/Equipo_04_main.py
@@ -1,3 +1,18 @@
+"""
+El programa lee varios archivos y les aplica los siguiente algoritmos:
+Algoritmo Z, Manahcer y Longest Common Substring
+
+Juan Eduardo Rosas Cerón - A01710168 
+Juan Carlos Calderón García - A01625696 
+Pablo Hazael Hurtado Mireles - A01710778 
+
+Creacion- 02//10/2024
+Modifcacion- 07/10/2024
+
+"""
+
+
+#La libreria pathlib toma la carpeta 
 from pathlib import Path
 
 script_location = Path(__file__).absolute().parent
@@ -33,45 +48,69 @@ transmission2 = ""
 for line in file.readlines():
     transmission2 += line.strip("\n")
 
-# Algoritmo Z
+
+'''
+Algoritmo Z
+
+Recibe un string concatenado con el patron que se tiene que buscar, un caracter especial que no se repita en el texto
+y un arreglo Z donde se guardaran las coinsidencias encontradas del patron dentro del texto
+
+Regresa ese mismo arreglo Z ya con todas las coincidencias encontradas
+'''
 def AlgoritmoZ(txt, Z):
     left = right = 0
 
     for i in range(len(txt)):
         if i > right:
+            # Se inicializan los valores donde empezara  a buscar considencias
             left = right = i
-            
+
+            # Se amplia los valores a buscar dentro del texto
             while right < len(txt) and txt[right - left] == txt[right]:
                 right += 1
 
+            # Se coloca un valor en la matriz Z correspondiente a las considencias encontradas
             Z[i] = right - left
             right -= 1 
+            
         else:
             k = i - left
 
+            # Se coloca un valor en la matriz Z correspondiente a las considencias encontradas
             if Z[k] < right - i + 1:
                 Z[i] = Z[k]
 
             else:
                 left = i
 
+                # Se amplia los valores a buscar dentro del texto
                 while right < len(txt) and txt[right - left] == txt[right]:
                     right += 1
 
-
+                # Se coloca un valor en la matriz Z correspondiente a las considencias encontradas
                 Z[i] = right - left
                 right -= 1
     
+"""
+Funcion auxiliar Search algoritmo Z
 
-# search algoritmo Z
+Recibe la transmision y el patron a buscar
+Devuelve en donde se encontro el patron o si no se encontro nada
+"""
 def search(trans, mcode):
     print("\nBuscando Archivo")
+    
+    # Se realiza la concatenación del patron a buscar y el texto donde se va a buscar
     conca = mcode + "$" + trans
+    
+    # Se crea el arreglo Z vacio que guardara el numero de considencias que se encuentre en el texto
     Z = [0] * len(conca)
     
+    # Se ejecuta la funcion de Algoritmo Z
     AlgoritmoZ(conca, Z)
-
     found = False
+    
+    # Esta parte solo imprime si se encontro el patron y en que parte 
     for i in range(len(conca)):
         if Z[i] == len(mcode):
             print(f"Patron encontrado en el indice:{i - len(mcode) - 1}")
@@ -80,7 +119,15 @@ def search(trans, mcode):
     if not found:
         print(f"Patrón no encontrado")
 
-# Manacher
+"""
+Algoritmo de Manacher
+Recibe el texto de una transmisión y busca en esta el palíndromo más largo
+Esto lo hace al ir avanzando de caracter en caracter y buscando un palíndromo centrado en ese caracter
+al comparar los caracteres a ambos lados y expandiendo el rango de búsqueda mientras haya coincidencias
+y almacenando en el arreglo palArray.
+Al encontrar un palíndromo aprovecha la propiedad simétrica de un palíndromo para utilizar los valores
+ya calculados para obtener los valores de caracteres dentro del palíndromo
+"""
 def Manacher(txt):
     n = len(txt)
     n = 2 * n + 1
@@ -125,7 +172,11 @@ def Manacher(txt):
     end = res + sizePal - 1
     return (int(res), int(end))
 
-# Longest Common Subsequence
+"""
+Longest Common Substring
+Recibe dos transmisiones y de estas de busca la substring mas larga de una transmison en la otra,
+finalmente si encuentra una substring con una longitud mayor a 1 devuelve la posicion de esta substring
+"""
 def lcs(X, Y):
     m = len(X)
     n = len(Y)
@@ -135,6 +186,7 @@ def lcs(X, Y):
     # Crear la tabla dp
     dp = [[0] * (n + 1) for _ in range(m + 1)]
 
+    #Recorre la lista de X, Y
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if X[i - 1] == Y[j - 1]:  # Si los caracteres coinciden
@@ -170,4 +222,4 @@ print(f"Transmisión 2: {LongPal2[0]} {LongPal2[1]}")
 # -> -> Parte 3
 result = lcs(transmission1, transmission2)
 print("Parte 3:")
-print(result)  # Salida: (0, 2) que corresponde a "ace"
+print(result)
